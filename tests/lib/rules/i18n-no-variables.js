19278f9f6bf2bd9f4b67dad3e44d94d085885e3f
Alex Kirk <alex.git@kirk.at>
2016-08-11 10:58:56 +0200

    Always return TemplateLiteral contents

--- tests/lib/rules/i18n-no-variables.js
+++ tests/lib/rules/i18n-no-variables.js
@@ -43,9 +43,6 @@ var rule = require( '../../../lib/rules/i18n-no-variables' ),
 		},
 		{
 			code: 'translate( `Hello World` );'
-		},
-		{
-			code: 'translate( `This is a string template` );'
 		}
 	],
 
