a5558d0c8047da254c74deee8ce5b78ffffbfb93
Julian de Bhal <julian.debhal@automattic.com>
2016-09-05 08:38:00 +1000

    Upgrade no-newlines to no-collapsible-whitespace
    Check for tabs and consecutive spaces in addition to newlines

--- tests/lib/rules/i18n-no-collapsible-whitespace.js
+++ tests/lib/rules/i18n-no-collapsible-whitespace.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Disallow using three dots in translate strings
+ * @fileoverview Disallow collapsible whitespace in translatable strings
  * @author Automattic
  * @copyright 2016 Automattic. All rights reserved.
  * See LICENSE.md file in root directory for full license.
@@ -10,7 +10,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require( '../../../lib/rules/i18n-no-newlines' ),
+var rule = require( '../../../lib/rules/i18n-no-collapsible-whitespace' ),
 	config = { env: { es6: true } },  // support for string templates
 	RuleTester = require( 'eslint' ).RuleTester;
 
@@ -18,7 +18,7 @@ var rule = require( '../../../lib/rules/i18n-no-newlines' ),
 // Tests
 //------------------------------------------------------------------------------
 
-( new RuleTester( config ) ).run( 'i18n-no-newlines', rule, {
+( new RuleTester( config ) ).run( 'i18n-no-collapsible-whitespace', rule, {
 	valid: [
 		{
 			code: 'this.translate( \'Hello World…\' );'
@@ -32,6 +32,10 @@ var rule = require( '../../../lib/rules/i18n-no-newlines' ),
 		{
 			code: "translate( '<p>Hello' + ' World!</p>' );"
 		},
+		{
+			code: 'translate( `A long string ` +\n `spread over ` +\n  `multiple lines.` );'
+		},
+
 	],
 
 	invalid: [
@@ -49,6 +53,36 @@ var rule = require( '../../../lib/rules/i18n-no-newlines' ),
 		},
 		{
 			code: 'translate( `My template literal\non two lines` );',
+			errors: [ {
+				message: rule.ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: "translate( '	My tab-indented string.' );",
+			errors: [ {
+				message: rule.ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: "translate( '\tMy string with a tab escape sequence.' );",
+			errors: [ {
+				message: rule.ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: "translate( '\u0009My string with a unicode tab.' );",
+			errors: [ {
+				message: rule.ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: 'translate( `A string with \r a carriage return.` );',
+			errors: [ {
+				message: rule.ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: "translate( 'A string with consecutive spaces.  These two are after a full stop.' );",
 			errors: [ {
 				message: rule.ERROR_MESSAGE
 			} ]
