715bfe3bed5ea293fecabecad49403a6c1bc2a92
Julian de Bhal <julian.debhal@automattic.com>
2016-09-05 11:36:39 +1000

    Make i18n-no-collapsible-whitespace messages less cryptic

--- tests/lib/rules/i18n-no-collapsible-whitespace.js
+++ tests/lib/rules/i18n-no-collapsible-whitespace.js
@@ -12,7 +12,16 @@
 
 var rule = require( '../../../lib/rules/i18n-no-collapsible-whitespace' ),
 	config = { env: { es6: true } },  // support for string templates
+	formatMessage = require( '../../util/format-message' ),
 	RuleTester = require( 'eslint' ).RuleTester;
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+const NO_NEWLINES = formatMessage( rule.ERROR_MESSAGE, { problem: ' (\\n)' } );
+const NO_CONSECUTIVE_SPACES = formatMessage( rule.ERROR_MESSAGE, { problem: ' (consecutive spaces)' } );
+const NO_TABS = formatMessage( rule.ERROR_MESSAGE, { problem: ' (\\t)' } );
 
 //------------------------------------------------------------------------------
 // Tests
@@ -42,49 +51,50 @@ var rule = require( '../../../lib/rules/i18n-no-collapsible-whitespace' ),
 		{
 			code: 'translate( "My double-quoted string\\nwith a newline" );',
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_NEWLINES,
+				problem: ' (\\n)'
 			} ]
 		},
 		{
 			code: "translate( 'My single quoted string\\nwith a newline' );",
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_NEWLINES
 			} ]
 		},
 		{
 			code: 'translate( `My template literal\non two lines` );',
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_NEWLINES
 			} ]
 		},
 		{
 			code: "translate( '	My tab-indented string.' );",
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_TABS
 			} ]
 		},
 		{
 			code: "translate( '\tMy string with a tab escape sequence.' );",
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_TABS
 			} ]
 		},
 		{
 			code: "translate( '\u0009My string with a unicode tab.' );",
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_TABS
 			} ]
 		},
 		{
 			code: 'translate( `A string with \r a carriage return.` );',
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_NEWLINES
 			} ]
 		},
 		{
 			code: "translate( 'A string with consecutive spaces.  These two are after a full stop.' );",
 			errors: [ {
-				message: rule.ERROR_MESSAGE
+				message: NO_CONSECUTIVE_SPACES
 			} ]
 		},
 	]
