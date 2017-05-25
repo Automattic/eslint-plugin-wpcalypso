c330a5389bb6fd6420a4ec370cce321c1c8b3d3a
Miguel Fonseca <miguelcsf@gmail.com>
2017-05-25 23:37:56 +0100

    Look for invocations of `connect` to inspect `mapState`
    - Instead of operating on any function that has `state` as an argument,
      only consider those that are actually passed as the first argument to
      `connect` — whether as an anonymous function or as a variable.
    
    - Longer comments in the source should guide the reader through the
      implementation.

---
+++ tests/lib/rules/redux-no-bound-selectors.js
@@ -0,0 +1,104 @@
+
+/**
+ * @fileoverview Disallow new functions inside `mapStateToProps`
+ * @author Automattic
+ * @copyright 2017 Automattic. All rights reserved.
+ * See LICENSE.md file in root directory for full license.
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require( '../../../lib/rules/redux-no-bound-selectors' ),
+	RuleTester = require( 'eslint' ).RuleTester;
+
+const REF = ' See wp-calypso#14024';
+const BIND_ERROR_MESSAGE = "Don't bind functions within `connect`." + REF;
+const FUNC_ERROR_MESSAGE = "Don't instantiate functions within `connect`." + REF;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+( new RuleTester() ).run( 'redux-no-bound-selectors', rule, {
+	valid: [
+		`createSelector( function( state ) {
+			return state.foos.map( function( foo ) {
+				return state.bars.items[ foo ];
+			} );
+		}, function( state ) {
+			return state.foos;
+		} )`,
+
+		`React.createClass( {
+			setFoo: function( foo ) {
+				this.setState( { foo: foo }, function( state ) {
+					action( state.bars );
+				} );
+			}
+		} )`,
+
+		`var mapState = function( state ) {
+			return {
+				getSite: getSite.bind( null, state ),
+				getFoos: function( id ) {
+					return getFoos( state, id );
+				}
+			};
+		};`,
+
+		`var mapState = function( state ) {
+			return {
+				getSite: getSite.bind( null, state ),
+				getFoos: function( id ) {
+					return getFoos( state, id );
+				}
+			};
+		};
+		connect( mapStateToProps );
+		`,
+	],
+
+	invalid: [
+		{
+			code: `connect( function( state ) {
+				return {
+					getSite: getSite.bind( null, state )
+				};
+			} )( Foo )`,
+			errors: [ {
+				message: BIND_ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: `connect( function( state ) {
+				return {
+					getFoos: function( id ) {
+						return getFoos( state, id );
+					}
+				};
+			} )( Foo )`,
+			errors: [ {
+				message: FUNC_ERROR_MESSAGE
+			} ]
+		},
+		{
+			code: `var mapState = function( state ) {
+				return {
+					getSite: getSite.bind( null, state ),
+					getFoos: function( id ) {
+						return getFoos( state, id );
+					}
+				};
+			};
+			connect( mapState );
+			`,
+			errors: [
+				{ message: BIND_ERROR_MESSAGE },
+				{ message: FUNC_ERROR_MESSAGE },
+			]
+		},
+	]
+} );
